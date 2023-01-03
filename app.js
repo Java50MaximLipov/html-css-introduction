ae2ddb1452dc3ac64d2cb2ab3ecbcaadf7ae97bb
MaximLipov <maxim.lipov@gmail.com>
2023-01-04 01:05:17 +0200

    HW-14 - Array methods

--- app.js
+++ app.js
@@ -1,69 +1,38 @@
-function getRandomNumber(min, max) {
-    return min + Math.trunc(Math.random() * (max - min + 1));
-}
-function getRandomMatrix(rows, columns, min, max) {
-    const matrix = [];
-    for (let i = 0; i < rows; i++) {
-        matrix.push([]);
-        for (let j = 0; j < columns; j++) {
-            matrix[i].push(getRandomNumber(min, max));
-        }
+//  HW-14 (1)
+function coloringString(string1, string2) {
+    let result = 'Strings length is not equal!';
+    if (string1.length == string2.length) {
+        arrayString1 = Array.from(string1);
+        arrayString2 = Array.from(string2);
+        result = arrayString2.map(function (char, index) {
+            let res = 'red';
+            let arrayString1Index = arrayString1.indexOf(char);
+            if (arrayString1Index != -1) {
+                res = 'yellow';
+                while (arrayString1Index <= arrayString1.length) {
+                    if (char === arrayString1[index]) res = 'green';
+                    arrayString1Index++;
+                }
+            }
+            return res;
+        });
     }
-    return matrix;
+    return result;
 }
-function getRandomNumberArray(length, min, max) {
-    const array = [];
-    for (let i = 0; i < length; i++) {
-        array.push(getRandomNumber(min, max));
-    }
-    return array;
+console.log(coloringString("pappy", "app"));
+console.log(coloringString("pappy", "apple"));
+console.log(coloringString("pappy", "pappy"));
+console.log(coloringString("hope", "pole"));
+
+//  HW-14 (2)
+function getNumbersWithDigitsAmount(digitsAmount, array) {
+    const result = array.filter(function (number) {
+        let res = number < 0 ? -1 : 1;
+        let absValue = Math.abs(number);
+        return ((absValue.toString()).length == digitsAmount ? res *= absValue : false);
+    });
+    return result;
 }
-
-//  HW-13 Get HTML <ul> string
-function getHtmlUl(randomArray) {
-    let htmlString = "";
-    const tempArray = ['<ul class="list_class">'];
-    const liClass = '<li class="item_class">';
-    for (let i = 0; i < randomArray.length; i++) {
-        const divClassType = randomArray[i] === 0 ? 'white' : 'black';
-        tempArray.push(liClass + "<div " + `class="${divClassType}"` + "></div></li>");
-    }
-    tempArray.push('</ul>');
-    htmlString = tempArray.join("");
-    return htmlString;
-}
-
-//  HW-13 Transposed Matrix
-function matrixTransp(matrix) {
-    let columns = matrix.length;
-    let rows = matrix[0].length;
-    let matrixResult = [];
-    let matrixTemporary = [];
-    for (let j = 0; j < rows; j++) {
-        matrixTemporary = [];
-        for (let i = 0; i < columns; i++) {
-            matrixTemporary.push(matrix[i][j]);
-        }
-        matrixResult.push(matrixTemporary);
-    }
-    return matrixResult;
-}
-
-//  Get HTML <ul> string TEST
-const ulArray1 = getRandomNumberArray(6, 0, 1);
-const ulArray2 = getRandomNumberArray(6, 0, 1);
-console.log("Random div color numbers array is: " + ulArray1);
-console.log("innerHTML string is: " + getHtmlUl(ulArray1));
-console.log("Random div color numbers array is: " + ulArray2);
-console.log("innerHTML string is: " + getHtmlUl(ulArray2));
-
-//  Transposed Matrix TEST
-const matrix1 = getRandomMatrix(3, 2, 1, 9);
-const matrix2 = getRandomMatrix(3, 3, 1, 6);
-const matrix3 = getRandomMatrix(3, 2, 1, 3);
-console.log("Test Matrix:       " + matrix1);
-console.log("Transposed matrix: " + matrixTransp(matrix1));
-console.log("Test Matrix:       " + matrix2);
-console.log("Transposed matrix: " + matrixTransp(matrix2));
-console.log("Test Matrix:       " + matrix3);
-console.log("Transposed matrix: " + matrixTransp(matrix3));
+console.log(getNumbersWithDigitsAmount(2, [10, 20, 30, -10, 11, 100]));
+console.log(getNumbersWithDigitsAmount(3, [10, 0, 256, 1028, -512]));
+console.log(getNumbersWithDigitsAmount(3, [1, 100, -100, 25, 1000]));
